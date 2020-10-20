5915af544de3781de46c710cf849eddabc3ad2e1
vbaobao <vbao.main@gmail.com>
2020-10-19 19:18:00 -0700

    Write functional instantiation of Stack

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,32 @@
 var Stack = function() {
-  var someInstance = {};
+  var someInstance = {
+  };
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var lastKey = 0;
+  var size = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[lastKey] = value;
+    lastKey++;
+    size++;
   };
 
   someInstance.pop = function() {
+    if (size > 0) {
+      lastKey--;
+      size--;
+    }
+    // Note: let keeps the value rather than reference to the object
+    let temp = storage[lastKey];
+    delete storage[lastKey];
+    return temp;
   };
 
   someInstance.size = function() {
+    return size;
   };
 
   return someInstance;

e9c4ef6bc495cad067ae0ad0b48d4c509b1941fa
vbaobao <vbao.main@gmail.com>
2020-10-21 14:33:09 -0700

    Completed pseudoclassical Stack

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,25 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.key = 0;
 };
 
 
+Stack.prototype.push = function(value) {
+  // Zero indexed
+  this[this.key] = value;
+  this.key++;
+};
+
+Stack.prototype.pop = function() {
+  if (this.key > 0) {
+    this.key--;
+  }
+  let temp = this[this.key];
+  delete this[this.key];
+  return temp;
+};
+
+Stack.prototype.size = function() {
+  return this.key;
+};

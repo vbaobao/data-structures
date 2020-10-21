760c31f7cfee6372a896f2c323c9928de8f33660
vbaobao <vbao.main@gmail.com>
2020-10-21 13:37:25 -0700

    Write skeleton stackMethod functions

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,18 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var stack = {};
+  var key = 0;
+  _.extend(stack, stackMethods);
+  return stack;
 };
 
 var stackMethods = {};
+
+stackMethods.push = function(value) {};
+
+stackMethods.pop = function() {};
+
+stackMethods.size = function() {};
 
 

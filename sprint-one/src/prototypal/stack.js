23279958a6c2193fbadd35b784105fe57faee865
vbaobao <vbao.main@gmail.com>
2020-10-21 14:14:46 -0700

    Write skeleton for prototypal Stack

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,15 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var stack = Object.create(stackMethods);
+  stack.key = 0;
+  return stack;
 };
 
 var stackMethods = {};
 
+stackMethods.push = function() {};
 
+stackMethods.pop = function() {};
+
+stackMethods.size = function () {};

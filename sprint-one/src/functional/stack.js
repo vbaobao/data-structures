a74f3e8301ec80286b39526554f60f78bfd97746
vbaobao <vbao.main@gmail.com>
2020-10-21 13:24:33 -0700

    Complete functional Stack

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,29 @@
 var Stack = function() {
-  var someInstance = {};
+  var someInstance = {
+  };
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var lastKey = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[lastKey] = value;
+    lastKey++;
   };
 
   someInstance.pop = function() {
+    if (lastKey > 0) {
+      lastKey--;
+    }
+    // Note: let keeps the value rather than reference to the object
+    let temp = storage[lastKey];
+    delete storage[lastKey];
+    return temp;
   };
 
   someInstance.size = function() {
+    return lastKey;
   };
 
   return someInstance;

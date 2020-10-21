7081b98d34219d1468089bec65dfb4a9ab0cd944
vbaobao <vbao.main@gmail.com>
2020-10-21 14:34:31 -0700

    Write skeleton for psuedoclassical Queue i Please enter the commit message for your changes. Lines starting

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,14 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.firstKey = 0;
+  this.lastKey = 0;
+  this.qSize = 0;
 };
 
 
+Queue.prototype.enqueue = function(value) {};
+
+Queue.prototype.dequeue = function() {};
+
+Queue.prototype.size = function() {};

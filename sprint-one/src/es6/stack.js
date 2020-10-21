e7502856c8fce1d811d6ebd4cb41ffcfcae1364c
vbaobao <vbao.main@gmail.com>
2020-10-21 14:45:01 -0700

    Complete ES6 Stack

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -3,15 +3,28 @@ class Stack {
   // but try not not reference your old code in writing the new style.
 
   //Note to self: No semi-colon at the ends on constructor and methods
-  
+
   constructor() {
     this.key = 0;
   }
 
-  push(value) {}
+  push(value) {
+    // Zero indexed
+    this[this.key] = value;
+    this.key++;
+  }
 
-  pop() {}
+  pop() {
+    if (this.key > 0) {
+      this.key--;
+    }
+    let temp = this[this.key];
+    delete this[this.key];
+    return temp;
+  }
 
-  size() {}
+  size() {
+    return this.key;
+  }
 
 }

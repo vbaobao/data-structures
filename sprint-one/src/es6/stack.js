4b247c757f2ba2b4b9b58216c60a4c19138c2d00
vbaobao <vbao.main@gmail.com>
2020-10-21 14:42:38 -0700

    Write skeleton for ES6 Stack

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -1,7 +1,17 @@
 class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  //Note to self: No semi-colon at the ends on constructor and methods
+  
   constructor() {
+    this.key = 0;
   }
+
+  push(value) {}
+
+  pop() {}
+
+  size() {}
 
 }

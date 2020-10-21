72294e38ee28a1b29e606293e1170693e8ebabe2
vbaobao <vbao.main@gmail.com>
2020-10-21 14:20:08 -0700

    Write prototypal queue skeleton

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,17 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var queue = Object.create(queueMethods);
+  queue.firstKey = 0;
+  queue.lastKey = 0;
+  queue.qSize = 0;
+  return queue;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(value) {};
 
+queueMethods.dequeue = function() {};
+
+queueMethods.size = function() {};

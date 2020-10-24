d15720a6e2be1e1ac183e96452b7ff7c0d46ef47
Anthony Sim <anthonyysim@gmail.com>
2020-10-24 16:44:01 -0700

    Complete Tree

--- sprint-two/spec/treeSpec.js
+++ sprint-two/spec/treeSpec.js
@@ -37,6 +37,7 @@ describe('tree', function() {
     tree.addChild(6);
     tree.children[0].addChild(7);
     tree.children[1].addChild(8);
+    console.log(tree);
     expect(tree.contains(7)).to.equal(true);
     expect(tree.contains(8)).to.equal(true);
   });
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -2,9 +2,7 @@ var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
   _.extend(newTree, treeMethods);
-
-  // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];
 
   return newTree;
 };
@@ -12,13 +10,35 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  this.children.push( Tree(value) );
 };
 
+
+
 treeMethods.contains = function(target) {
+
+  let helperFunc = function(object, target) {
+    if (object.value === target) {
+      return true;
+
+    } else if (object.children.length) {
+      for (let child of object.children) {
+
+        if (helperFunc(child, target)) {
+          return true;
+        }
+      }
+    }
+    return false;
+  };
+
+  return helperFunc(this, target);
 };
 
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ addChild() --> O(1)
+ contain() -->  O(n^2)???
  */

7b3f936219923f9dd5456bd2f7d0a23962981fc8
Anthony Sim <anthonyysim@gmail.com>
2020-10-27 19:17:17 -0700

    Complete Set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,26 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = [];
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this._storage.push(item);
 };
 
 setPrototype.contains = function(item) {
+  return this._storage.includes(item);
 };
 
 setPrototype.remove = function(item) {
+  this._storage = this._storage.filter(e => e !== item);
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * .add() --> O(1)
+ * .contains() --> O(n)
+ * .remove() --> O(n)
  */

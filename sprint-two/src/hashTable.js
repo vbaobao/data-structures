2dd3f1e3fea48a9d7a4c55ebca0505993a6e97bd
Anthony Sim <anthonyysim@gmail.com>
2020-10-28 20:50:56 -0700

    almost completed hashTable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,5 +1,3 @@
-
-
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
@@ -7,19 +5,39 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.set(index, v);
+  if (this._storage.get(index) === undefined) {
+    this._storage.set(index, []);
+  }
+
+  for (const i of this._storage.get(index)) {
+    if (i[0] === k) {
+      i[1] = v;
+      return;
+    }
+  }
+  this._storage.get(index).push([k, v]);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  return this._storage.get(index);
+
+  for (const i of this._storage.get(index)) {
+    if (i[0] === k) {
+
+      return i[1];
+    }
+  }
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.each(function(val, i, storage) {
+  this._storage.each(function(bucket, i, storage) {
     if (i === index) {
-      storage.splice(i, 1);
+      for (const [tupIndex, tuple] of bucket.entries()) {
+        if (tuple[0] === k) {
+          bucket.splice(tuple, 1); // WIP
+        }
+      }
     }
   });
 };

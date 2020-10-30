1bf865bd40f6b25ce281e7ccc8a95268ff94e6a3
Anthony Sim <anthonyysim@gmail.com>
2020-10-30 14:37:49 -0700

    refactored linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,60 +1,3 @@
-var LinkedList = function() {
-  var list = {};
-  list.head = null;
-  list.tail = null;
-  /*
-  let node1 = {
-    value: 1,
-    next: {
-      value: 2,
-      next = null;
-    }
-  }
-  */
-
-  list.addToTail = function(value) {
-    let instance = new Node(value);
-
-    if (!this.head) {
-      this.head = instance;
-      this.tail = this.head;
-    } else {
-      this.tail.next = instance;
-      this.tail = instance;
-    }
-  };
-
-  list.removeHead = function() {
-    // if head doesn't exit?! then return false.
-    if (this.head === undefined) {
-      return false;
-    }
-    // if head does EXIST, we would set this.head should first be assigned to a separate variable.
-    let temp = this.head;
-
-    // then this.head should reference this.head.next
-    this.head = this.head.next;
-    return temp.value;
-  };
-
-  list.contains = function(target) {
-    // Helper function
-    let recursiveSearch = function (object, target) {
-      if (object.value === target) {
-        return true;
-      } else if (object.next === null) {
-        return false;
-      } else {
-        return recursiveSearch(object.next, target);
-      }
-    };
-
-    return recursiveSearch(this.head, target);
-  };
-
-  return list;
-};
-
 var Node = function(value) {
   var node = {};
 
@@ -63,6 +6,56 @@ var Node = function(value) {
 
   return node;
 };
+
+var LinkedList = function() {
+  var list = {};
+  list.head = null;
+  list.tail = null;
+
+  list.addToTail = function(value) {
+    let instance = new Node(value);
+
+    if (!this.head) {
+      this.head = instance;
+      this.tail = this.head;
+
+    } else {
+      this.tail.next = instance;
+      this.tail = instance;
+    }
+  };
+
+  list.removeHead = function() {
+    if (!this.head) {
+      return false;
+    }
+    let temp = this.head;
+
+    this.head = this.head.next;
+
+    return temp.value;
+  };
+
+  list.contains = function(target) {
+    if (!this.head) {
+      return false;
+    }
+
+    while (this.head !== null) {
+      if (this.head.value === target) {
+        return true;
+
+      } else {
+        this.head = this.head.next;
+      }
+    }
+
+    return false;
+  };
+
+  return list;
+};
+
 
 /*
  * Complexity: What is the time complexity of the above functions?

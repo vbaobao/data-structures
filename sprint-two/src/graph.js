679e8c0ad33b2d5005754f784e72735252625a38
Anthony Sim <anthonyysim@gmail.com>
2020-10-31 15:01:24 -0700

    added test graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -51,6 +51,10 @@ Graph.prototype.removeEdge = function(fromNode, toNode) {
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
+  if (!cb) {
+    return false;
+  }
+
   for (let key in this.nodes) {
     cb(Number(key));
   }

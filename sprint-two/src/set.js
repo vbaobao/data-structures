b1d0db307d58099dc7a59bdacf219ae90dd708fe
Anthony Sim <anthonyysim@gmail.com>
2020-10-31 09:39:19 -0700

    Complete Set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,26 +1,54 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = [];
+  set._limit = 8;
+  set._storage = LimitedArray(set._limit);
+
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
-  this._storage.push(item);
+  var index = getIndexBelowMaxForKey(item, this._limit);
+  let bucket = this._storage.get(index);
+
+  if (bucket === undefined) {
+    bucket = [];
+    this._storage.set(index, bucket);
+  }
+
+  bucket.push([item, item]);
 };
 
 setPrototype.contains = function(item) {
-  return this._storage.includes(item);
+  var index = getIndexBelowMaxForKey(item, this._limit);
+  let bucket = this._storage.get(index);
+
+  for (const i of bucket) {
+    if (i[0] === item) {
+      return true;
+    }
+  }
+  return false;
 };
 
 setPrototype.remove = function(item) {
-  this._storage = this._storage.filter(e => e !== item);
+  var index = getIndexBelowMaxForKey(item, this._limit);
+
+  this._storage.each(function(bucket, i) {
+    if (i === index) {
+      for (const [tupIndex, tuple] of bucket.entries()) {
+        if (tuple[0] === item) {
+          bucket.splice(tupIndex, 1);
+        }
+      }
+    }
+  });
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
  * .add() --> O(1)
- * .contains() --> O(n)
- * .remove() --> O(n)
+ * .contains() --> O(1)
+ * .remove() --> O(1)
  */
